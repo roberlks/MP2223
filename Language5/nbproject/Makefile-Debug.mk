b7ab3abac12f9c65e15e05d4ed68f6f0b5f88253
roberlks <e.roberlks222@go.ugr.es>
2023-06-01 13:36:41 +0200

    Revert "NONONO"
    This reverts commit a67656854052412fe4550b723b8a681289808730.

--- Language5/nbproject/Makefile-Debug.mk
+++ Language5/nbproject/Makefile-Debug.mk
@@ -21,8 +21,8 @@ FC=gfortran
 AS=as
 
 # Macros
-CND_PLATFORM=MinGW-Windows
-CND_DLIB_EXT=dll
+CND_PLATFORM=GNU-Linux
+CND_DLIB_EXT=so
 CND_CONF=Debug
 CND_DISTDIR=dist
 CND_BUILDDIR=build
@@ -60,9 +60,9 @@ LDLIBSOPTIONS=
 
 # Build Targets
 .build-conf: ${BUILD_SUBPROJECTS}
-	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/language5.exe
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/language5
 
-${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/language5.exe: ${OBJECTFILES}
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/language5: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/language5 ${OBJECTFILES} ${LDLIBSOPTIONS}
 
